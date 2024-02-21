cd48b0e9d020d4d56e44ba93d0deb69fcbb964c7
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-02-21 14:53:35 +0500

    feat(): connect the authenticate micro service with broher service

---
+++ authentication-service/cmd/api/handlers.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, req)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate user against database
+	user, err := app.Models.User.GetByEmail(req.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credientails"), http.StatusBadRequest)
+		return
+	}
+
+	valid, err := user.PasswordMatches(req.Password)
+	if err != nil || !valid {
+		app.errorJSON(w, errors.New("invalid credientails"), http.StatusBadRequest)
+		return
+	}
+
+	payload := JsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("logged in user %s", user.Email),
+		Data:    user,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,7 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
+)
+
+type (
+	RequestPayload struct {
+		Action string      `json:"action"`
+		Auth   AuthPayload `json:"auth"`
+	}
+
+	AuthPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 )
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +26,72 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmit(w http.ResponseWriter, r *http.Request) {
+	var req RequestPayload
+
+	err := app.readJSON(w, r, req)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	switch req.Action {
+	case "auth":
+		app.authenticate(w, req.Auth)
+	default:
+		app.errorJSON(w, errors.New("unknown action"))
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+
+	// call the service
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// check statius code
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service "))
+		return
+	}
+
+	// json from service
+	data := JsonResponse{}
+
+	// decode json from service
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if data.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	payload := JsonResponse{
+		Error:   data.Error,
+		Message: "Authenticated",
+		Data:    data.Data,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
